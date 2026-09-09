cb7bdd2a74f8f26262dcb4c8f0e49d75427a8dde
sheiksallu05 <sheiksalman702@gmail.com>
2026-09-09 10:31:58 +0530

    added a new react file named para.jsx and modify the main.jsx file

---
+++ REACT/day1app/src/Para.jsx
@@ -0,0 +1,14 @@
+function Para() {
+    return (
+        <>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque a, 
+               ipsam voluptas tempore neque obcaecati consequuntur quibusdam quas 
+               voluptatum ipsa sequi veritatis laudantium voluptate recusandae 
+               consequatur repudiandae esse nihil praesentium.</p>
+        </>
+
+    );
+
+}
+
+export default Para;
